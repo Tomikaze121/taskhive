cb87575249f235e78902498f87a6c58a94ed5453
JC <Jeanclaude.lequigan.22@usjr.edu.ph>
2025-06-24 22:05:45 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taskhive',
+  webDir: 'www'
+};
+
+export default config;
